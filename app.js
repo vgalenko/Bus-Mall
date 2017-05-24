dbe54c4e95b9e4e14201cb941f31689b2638243e
vgalenko <vitaliygalenko@gmail.com>
2017-05-23 22:14:38 -0700

    added sudo code to my js file and converted my object constructors

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
 'use strict';
 
 var totalCounter = 0; // this will count up the total of clicked images
+var totalImages = [];
+var totalClicked = [];
 
-var images = ['bag', 'bannana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
-var path = 'img/bag.jpg','img/banana.jpg','img/bathroom.jpg','img/boots.jpg','img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg',
+function Image(name, path) {
+this.name = name
+this.path = path;
+this.shown =0;
+this.totalClicked = 0;
+totalImages.push(this);
+}
 
 var bag = new Image('bag', 'img/bag.jpg');
 var bannana = new Image('bannana', 'img/banana.jpg');
 var bathroom = new Image('bathroom', 'img/bathroom.jpg');
 var boots = new Image('boots', 'img/boots.jpg');
 var breakfast = new Image('breakfast', 'img/breakfast.jpg');
+var bubblegum = new Image('bubblegum', 'img/bubblegum.jpg');
+var chair = new Image('chair', 'img/chair.jpg');
+var cthulhu = new Image('cthulhu', 'img/cthulhu.jpg');
+var dogduck = new Image('dog-duck', 'img/dog-duck.jpg');
+var dragon = new Image('dragon', 'img/dragon.jpg');
+var pen = new Image('pen', 'img/pen.jpg');
+var pet_sweep = new Image('pet-sweep', 'img/pet-sweep.jpg');
+var scissors = new Image('scissors', 'img/scissors.jpg');
+var shark = new Image('shark', 'img/shark.jpg');
+var sweep = new Image('sweep', 'img/sweep.png');
+var tauntaun = new Image('tauntaun', 'img/tauntaun.jpg');
+var unicorn = new Image('unicorn', 'img/unicorn.jpg');
+var usb = new Image('usb', 'img/usb.gif');
+var water_can = new Image('water-can', 'img/water-can.jpg');
+var wine_glass = new Image('wine-glass', 'img/wine-glass.jpg');
 
 
-
-function Image(images, path, shown, clicked) {
-this.name = name
-this.path = path;
-this.shown =0;
-this.clicked = 0;
+function randomGenerator() {
+  return Math.floor(Math.random() * totalImages.length);
 }
 
-
-
-function randomGenerator () {
-  return Math.floor(Math.random() * images.length);
-}
-
-var totalImgs = [];
+//I need to create a function for images to appear on the browser by using the randomGenerator function
+// I might need 3 functions for 3 seperate pictures.
+// I need to create a function that calculates the number of clicks I made
+// I need a function that will count what pictures I chose and another function to store pictures i didn't chose.
+// I need an eventListener so that verytime I click on a pictures it would generate new pics.
+// I need to get elementById
+// I will need to call functions in other functions 
